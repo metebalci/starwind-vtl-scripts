54ec142ebf5f341cdd6cb1531e1772e25ccf4fe3
Mete Balci <metebalci@gmail.com>
2025-10-24 18:05:41 +0200

    target and library scripts

---
+++ README.md
@@ -0,0 +1,9 @@
+# StarWind VTL Example PowerShell Scripts
+
+Device is called Library.
+
+## Object Model
+
+- Server owns Targets and Devices.
+- Target has associated Devices.
+- Device has Tapes.
---
+++ defaults.ps1
@@ -0,0 +1,57 @@
+# defaults
+
+$defaultAddr = [IPAddress]"127.0.0.1"
+$defaultPort = 3261
+$defaultUser = "root"
+$defaultPass = "starwind"
+
+$defaultTargetAlias = "mytarget"
+$defaultLibraryName = "myvtl"
+$defaultLibraryPath = "My Computer\C\starwind"
+$defaultLibraryPatternIndex = 0
+
+# constants
+
+$STARWINDS_HEADERS_FOLDER = "C:\Program Files\StarWind Software\StarWind\headers"
+
+# below the actual variables are set from defaults if not given by the user
+
+if ((Test-Path variable:addr) -and ($null -eq $addr))
+{
+	$addr = $defaultAddr
+}
+
+if ((Test-Path variable:port) -and ($null -eq $port))
+{
+	$port = $defaultPort
+}
+
+if ((Test-Path variable:user) -and ($null -eq $user))
+{
+	$user = $defaultUser
+}
+
+if ((Test-Path variable:pass) -and ($null -eq $pass))
+{
+	$pass = $defaultPass
+}
+
+if ((Test-Path variable:targetAlias) -and ($null -eq $targetAlias))
+{
+	$targetAlias = $defaultTargetAlias	
+}
+
+if ((Test-Path variable:libraryName) -and ($null -eq $libraryName))
+{
+	$libraryName = $defaultLibraryName
+}
+
+if ((Test-Path variable:libraryPath) -and ($null -eq $libraryPath))
+{
+	$libraryPath = $defaultLibraryPath
+}
+
+if ((Test-Path variable:libraryPatternIndex) -and ($null -eq $libraryPatternIndex))
+{
+	$libraryPatternIndex = $defaultLibraryPatternIndex
+}
